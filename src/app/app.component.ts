a211325c0629d00ee989e5a0e3f33a1c7b8b3d31
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2020-03-20 16:24:25 +0800

    Fixed back key navigation to exit app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform } from '@ionic/angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, IonRouterOutlet } from '@ionic/angular';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { BlockchainService } from './services/blockchain.service';
@@ -10,6 +10,8 @@ import { Router } from '@angular/router';
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(IonRouterOutlet, {static: true}) routerOutlet: IonRouterOutlet;
+  
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
@@ -26,7 +28,23 @@ export class MyApp {
       this.splashScreen.hide();
       this.blockchain.init();
 
+      this.setupBackKeyNavigation();
+
       this.router.navigate(["menu/blocks"]);
+    });
+  }
+
+  /**
+   * Listen to back key events. If the default router can go back, just go back.
+   * Otherwise, exit the application.
+   */
+  setupBackKeyNavigation() {
+    this.platform.backButton.subscribeWithPriority(0, () => {
+      if (this.routerOutlet && this.routerOutlet.canGoBack()) {
+        this.routerOutlet.pop();
+      } else {
+        navigator['app'].exitApp();
+      }
     });
   }
 }
